f65b8c1ec002e5cedb07aa1aa061f9bbe0e3e6ff
AlexLosciale <losciale17@gmail.com>
2025-02-12 18:20:24 +0100

    add:data

--- ROUTERS/blog.js
+++ ROUTERS/blog.js
@@ -1,2 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const arreyposts = require('../data/blogs');
+
+// Index: Lista di tutti i blog
+router.get('/', (req, res) => {
+    res.json(arreyposts);
+});
+
+// Show: Dettagli di un singolo blog
+router.get('/:id', (req, res) => {
+    res.send(`Dettagli del blog ${req.params.id}`);
+});
+
+// Create: Creazione di un nuovo blog
+router.post('/', (req, res) => {
+    res.send('Creazione di un nuovo blog');
+});
+
+// Update: Modifica integrale di un blog
+router.put('/:id', (req, res) => {
+    res.send(`Modifica del blog ${req.params.id}`);
+});
+
+// Delete: Eliminazione di un blog
+router.delete('/:id', (req, res) => {
+    res.send(`Cancellazione del blog ${req.params.id}`);
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,12 +2,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const blogRouter = require('./ROUTERS/blog');
+const bachecaRouter = require('./data/blogs');
+
 app.use(express.static('public'));
+app.use("/blog", blogRouter);
+app.use("/", bachecaRouter);
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send('Hello World');
+});
+
+// index
+app.get('/blog', function (req, res) {
+res.send('Lista dei blog');
+});
+// show
+app.get('/blog/:id', function (req, res) {
+res.send('Dettagli dei blog ' + req.params.id);
+});
+// store
+app.post('/blog', function (req, res) {
+res.send('Creazione nuovo blog');
+});
+// update
+app.put('/blog/:id', function (req, res) {
+res.send('Modifica integrale del blog ' + req.params.id);
+});
+// modify
+app.patch('/blog/:id', function (req, res) {
+res.send('Modifica parziale del blog ' + req.params.id);
+});
+// destroy
+app.delete('/blog/:id', function (req, res) {
+res.send('Eliminazione del blog ' + req.params.id);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
