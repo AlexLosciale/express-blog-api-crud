50ba3de5a5c307e2131e017c5ca3a14bbe237580
AlexLosciale <losciale17@gmail.com>
2025-02-13 09:34:16 +0100

    add:finish

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const port = 3000;
 
 const blogRouter = require('./ROUTERS/blog'); 
-// Rimuoviamo l'importazione di bachecaRouter perché non è un router, ma un array di dati
 
 app.use(express.static('public'));
 app.use(express.json()); 
 
-app.use("/blog", blogRouter);  // Usa solo il router per i blog
+app.use("/blog", blogRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
