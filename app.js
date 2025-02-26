c39c23c3e4e978d25a14217f741177f176792e9a
AlexLosciale <losciale17@gmail.com>
2025-02-26 17:24:41 +0100

    add:cors

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+    }
+));
 
 app.use(express.json()); 
 app.use(express.static('public'));
